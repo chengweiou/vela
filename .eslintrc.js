a7ab09ba259b2ce210fa2d64dde88fa968483381
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-09-03 21:56:16 -0700

    更新一些找到的问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
   settings: { // @路径引入的文件也需要校验
     'import/resolver': {
       alias: {
@@ -9,15 +19,6 @@ module.exports = {
       },
     },
   },
-
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "prettier",
-  ],
   rules: {
     // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -32,10 +33,12 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 0, 'maxBOF': 0 }],
     "indent": ["error", 2, { "SwitchCase": 1 }],
+    // todo 这里先这样吧，到时候在考虑去掉
+    "vue/script-setup-uses-vars": "off",
   },
-  // parserOptions: {
-  //   parser: 'babel-eslint',
-  // },
+  parserOptions: {
+    ecmaVersion: '2021',
+  },
   // overrides: [
   //   {
   //     files: [
